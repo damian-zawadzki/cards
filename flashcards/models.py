c44a4f8caef278c240aaa4d0d38360b9f3df021b
damian-zawadzki <dam.zawadzki@gmail.com>
2022-05-27 15:29:48 +0200

    null

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 
+
 class Card(models.Model):
-	card_id = models.IntegerField(primary_key = True)
-	client = models.CharField(max_length=200)
-	deck = models.CharField(max_length=200)
-	english = models.CharField(max_length=200)
-	polish = models.CharField(max_length=200)
-	publication_date = models.IntegerField()
-	due_date = models.IntegerField()
-	interval = models.IntegerField()
-	number_of_reviews = models.IntegerField()
-	answers = models.CharField(max_length=200)
-	card_opening_times = models.CharField(max_length=200)
-	card_closing_times = models.CharField(max_length=200)
-	durations = models.CharField(max_length=200)
-	card_revision_days = models.CharField(max_length=200)
-	line = models.IntegerField()
+    card_id = models.IntegerField(primary_key=True)
+    client = models.CharField(max_length=200)
+    deck = models.CharField(max_length=200)
+    english = models.CharField(max_length=200)
+    polish = models.CharField(max_length=200)
+    publication_date = models.IntegerField()
+    due_date = models.IntegerField()
+    interval = models.IntegerField()
+    number_of_reviews = models.IntegerField()
+    answers = models.CharField(max_length=200, null=True)
+    card_opening_times = models.CharField(max_length=200, null=True)
+    card_closing_times = models.CharField(max_length=200, null=True)
+    durations = models.CharField(max_length=200, null=True)
+    card_revision_days = models.CharField(max_length=200, null=True)
+    line = models.IntegerField()
+
 
 class Client(models.Model):
-	client = models.CharField(max_length=200)
-	daily_limit_of_new_cards = models.IntegerField()
+    client = models.CharField(max_length=200)
+    daily_limit_of_new_cards = models.IntegerField()
